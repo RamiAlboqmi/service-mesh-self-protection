27f838fb4230806b688862e4a0b44b2ad10bb295
RamiAlboqmi <108428419+RamiAlboqmi@users.noreply.github.com>
2022-07-14 05:39:23 -0500

    Update UserRepository.java

--- AAF/src/main/java/com/UserRepository.java
+++ AAF/src/main/java/com/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository <Storeservicerequest,Long> {
+public interface StoreservicerequestRepository extends JpaRepository <Storeservicerequest,Long> {
     List<Storeservicerequest> findByUsername(String username);
     // User findByRequestid(String requestid);
     List <Storeservicerequest> findByRequestid (String requestid);
